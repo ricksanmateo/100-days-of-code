b850505cba97f34e8c3bc02d16fdfaff54c02140
rick <ricksanmateo27@gmail.com>
2025-08-21 19:49:36 +0800

    day 2

---
+++ day_2/task_1.py
@@ -0,0 +1,23 @@
+print("hello"[0])
+
+print(len("hello"))
+
+print(type("1234.45"))
+print(type(1234))
+print(type(1234.45))
+print(type(True))
+
+# type casting
+print(int("1234"))
+print(float("1234.45"))
+print(str(1234))
+
+print(int("1234") + int("1234"))
+
+print(123 - 12)
+print(123 + 12)
+print(123 * 12)
+print(123 / 12)
+print(123 // 12)
+print(123 % 12)
+print(123 ** 12)
