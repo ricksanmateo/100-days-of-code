55b75a3328b23e596c3dbda370df4e97a089c4e5
rick <ricksanmateo27@gmail.com>
2025-08-23 18:07:42 +0800

    day 3

---
+++ day_3/task_2.py
@@ -0,0 +1,8 @@
+# Modulo
+
+number_to_checkint = int(input("What is the number you want to check? "))
+
+if number_to_checkint % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
